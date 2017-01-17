714db8a6fc30c665acbff56765b33bbf60ea6705
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-01-17 16:31:28 +0100

    setup.py 0.1.1

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ def read(file_):
 
 setup(
   name = 'hyper-engine',
-  version = '0.1.0',
+  version = '0.1.1',
   author = 'Maxim Podkolzine',
   author_email = 'maxim.podkolzine@gmail.com',
   description = 'Python library for hyper-parameters optimization',
   license = 'Apache 2.0',
   keywords = 'machine learning, hyper-parameters, model selection, bayesian optimization',
   url = 'https://github.com/maxim5/hyper-engine',
-  packages=find_packages(exclude=['tests']),
+  packages=find_packages(exclude=['*tests*']),
   long_description=read('README.rst'),
 )
