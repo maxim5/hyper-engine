1efde5f86398b0ab4554131feb406905f28b5b85
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-01-20 14:50:41 +0100

    python-3.x print

--- hyperengine/base/logging.py
+++ hyperengine/base/logging.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 __author__ = 'maxim'
 
 
@@ -8,7 +9,7 @@ LOG_LEVEL = 1
 
 def log(*msg):
   import datetime
-  print '[%s]' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ' '.join([str(it) for it in msg])
+  print('[%s]' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ' '.join([str(it) for it in msg]))
 
 def set_silence():
   global LOG_LEVEL
