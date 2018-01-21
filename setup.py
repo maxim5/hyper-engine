3e5858366297e61de0d6f2cd0096a04c2f0be4eb
Maxim Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-01-21 14:07:53 +0100

    setup.py: split requirements into mandatory and extra

--- setup.py
+++ setup.py
@@ -13,11 +13,14 @@ setup(
   version = '0.1.2',
   author = 'Maxim Podkolzine',
   author_email = 'maxim.podkolzine@gmail.com',
-  description = 'Python library for hyper-parameters optimization',
+  description = 'Python library for Bayesian hyper-parameters optimization',
   license = 'Apache 2.0',
   keywords = 'machine learning, hyper-parameters, model selection, bayesian optimization',
   url = 'https://github.com/maxim5/hyper-engine',
-  packages=find_packages(exclude=['*tests*']),
-  long_description=read('README.rst'),
-  requires=['numpy', 'scipy', 'tensorflow'],
+  packages = find_packages(exclude=['*tests*']),
+  long_description = read('README.rst'),
+  install_requires = ['numpy', 'scipy'],
+  extras_require = {
+    'tf': ['tensorflow'],
+  },
 )
