8ecbda036de086aca2d88937531c23e1a4f6d129
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-01-25 19:04:18 +0100

    import more stuff to the world

--- hyperengine/__init__.py
+++ hyperengine/__init__.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'maxim'
 
-from .base.base_io import BaseIO, DefaultIO, Serializable
 from .bayesian import *
 from .model import *
 from .impl.tensorflow import *
 
+import base as util
 import spec
--- hyperengine/base/__init__.py
+++ hyperengine/base/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'maxim'
 
-from .logging import log, set_silence, set_verbose, is_info_logged, debug, info, warn, vlog, vlog2, vlog3, log_at_level
+from .logging import log, set_silence, set_verbose, is_info_logged, is_debug_logged, debug, info, warn, vlog, vlog2, vlog3, log_at_level
 from .util import smart_str, str_to_dict, zip_longest, deep_update, mini_batch, random_id, safe_concat, call, slice_dict, as_function, as_numeric_function
 from .named_dict import NamedDict
 from .base_io import BaseIO, DefaultIO, Serializable
