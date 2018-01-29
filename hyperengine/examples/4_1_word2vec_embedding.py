0c9915b94a4e461e9cea3fc45bf577f99c67016d
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-01-29 14:08:55 +0100

    code reuse

--- hyperengine/examples/4_1_word2vec_embedding.py
+++ hyperengine/examples/4_1_word2vec_embedding.py
@@ -20,18 +20,9 @@ class SentenceDataProvider(hype.IterableDataProvider):
     self._num_skips = params.get('num_skips', 2)
     self._skip_window = params.get('skip_window', 1)
 
-    self._step = 0
-    self._index = 0
-    self._epochs_completed = 0
-    self._just_completed = False
-
     self._vocabulary = None
     self._dictionary = None
     self._data = None
-
-  @property
-  def size(self):
-    return len(self._data)
 
   @property
   def vocabulary(self):
@@ -41,28 +32,6 @@ class SentenceDataProvider(hype.IterableDataProvider):
   def vocab_size(self):
     return self._vocab_size
 
-  @property
-  def step(self):
-    return self._step
-
-  @property
-  def index(self):
-    return self._index
-
-  @property
-  def epochs_completed(self):
-    return self._epochs_completed
-
-  @property
-  def just_completed(self):
-    return self._just_completed
-
-  def reset_counters(self):
-    self._step = 0
-    self._index = 0
-    self._epochs_completed = 0
-    self._just_completed = False
-
   def build(self):
     hype.util.debug('Building the data provider')
     words = get_text8('temp-text8/data')
@@ -70,6 +39,7 @@ class SentenceDataProvider(hype.IterableDataProvider):
     self._vocabulary = np.array([word for word, _ in self._vocabulary])
     self._dictionary = {word: code for code, word in enumerate(self._vocabulary)}
     self._data = np.array([self._dictionary.get(word, 0) for word in words])
+    self._size = len(self._data)
 
     if hype.util.is_debug_logged():
       hype.util.debug('Total words in text: %dM' % (len(words) / 1000000))
@@ -89,7 +59,7 @@ class SentenceDataProvider(hype.IterableDataProvider):
     span = 2 * skip_window + 1  # [ skip_window target skip_window ]
     buffer = deque(maxlen=span)
     for _ in range(span):
-      buffer.append(self._data[self._index])
+      buffer.append(self._data[self._index_in_epoch])
       self._inc_index()
     for i in range(batch_size // num_skips):
       target = skip_window  # target label at the center of the buffer
@@ -100,19 +70,9 @@ class SentenceDataProvider(hype.IterableDataProvider):
         targets_to_avoid.append(target)
         batch[i * num_skips + j] = buffer[skip_window]
         labels[i * num_skips + j, 0] = buffer[target]
-      buffer.append(self._data[self._index])
+      buffer.append(self._data[self._index_in_epoch])
       self._inc_index()
     return batch, labels
-
-  def _inc_index(self):
-    next = self._index + 1
-    if next >= len(self._data):
-      self._index = 0
-      self._epochs_completed += 1
-      self._just_completed = True
-    else:
-      self._index = next
-      self._just_completed = False
 
 
 def word2vec_model(params):
