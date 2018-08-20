d9c8c30f631945ca8d89a3c72048d17b73e78108
KOLANICH <KOLANICH@users.noreply.github.com>
2018-08-20 09:57:23 +0000

    Added iterations count limit
    Of course it can be done wia hacks with exceptions, but I dislike that way.

--- hyperengine/model/hyper_tuner.py
+++ hyperengine/model/hyper_tuner.py
@@ -19,7 +19,7 @@ strategies = {
 
 
 class HyperTuner(object):
-  def __init__(self, hyper_params_spec, solver_generator, **strategy_params):
+  def __init__(self, hyper_params_spec, solver_generator, iterations_limit=float("inf"), **strategy_params):
     self.solver_generator = solver_generator
 
     self.parsed = ParsedSpec(hyper_params_spec)
@@ -31,17 +31,20 @@ class HyperTuner(object):
     strategy_gen = as_function(strategy_params.get('strategy', 'bayesian'), presets=strategies)
     self.strategy = strategy_gen(sampler, strategy_params)
 
+    self.iterations_limit = iterations_limit
     self.timeout = strategy_params.get('timeout', 0)
 
   def tune(self):
     info('Start hyper tuner')
 
-    while True:
+    i=0
+    while i < iterations_limit:
       point = self.strategy.next_proposal()
       hyper_params = self.parsed.instantiate(point)
       solver = self.solver_generator(hyper_params)
 
       accuracy = solver.train()
+      i+=1
 
       previous_max = np.max(self.strategy.values) if len(self.strategy.values) > 0 else -np.inf
       self.strategy.add_point(point, accuracy)
